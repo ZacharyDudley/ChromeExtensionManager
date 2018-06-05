8ff7e0bbe36018415938d34bdab521b220094226
Zachary Dudley Friedman <ZacharyDFriedman@gmail.com>
2018-06-05 10:22:20 -0500

    clicking button next to extension name toggles extension on and off

--- menu.js
+++ menu.js
@@ -7,6 +7,8 @@ chrome.management.getAll(function(allExtensions) {
       let title = document.createTextNode(extension.shortName);
       let button = document.createElement('button');
 
+      button.addEventListener('mouseup', () => sendMessageToBackground(extension.id));
+
       extensionItem.id = `${extension.id}`;
       extensionItem.appendChild(title);
       extensionItem.appendChild(button);
@@ -15,23 +17,10 @@ chrome.management.getAll(function(allExtensions) {
   });
 });
 
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, {greeting: 'hello'}, function(response) {
-    console.log('TAB RESPONSE: ', response)
-  })
-})
-
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log('REQ: ', request)
-    console.log('SENDER: ', sender)
-});
-
-document.addEventListener('load', function() {
-  let allButtons = window.document.getElementsByTagName('button');
-  for (let i = 0; i < allButtons.length; i++) {
-    allButtons[i].addEventListener('click', function(event) {
-      console.log('EVNT: ', event)
+function sendMessageToBackground(extensionId) {
+  chrome.management.get(extensionId, function(extensionInfo) {
+    chrome.runtime.sendMessage({id: extensionId, enabled: extensionInfo.enabled}, function(response) {
+      console.log('MENU SCRIPT')
     })
-  }
-})
+  })
+}
